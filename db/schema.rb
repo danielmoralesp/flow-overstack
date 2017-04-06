913583ad19e5ddf78d0e74c289f1d7087714e633
danielmoralesp <daniel_moralesp@hotmail.com>
2017-04-06 13:19:54 -0500

    latest migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150929201728) do
+ActiveRecord::Schema.define(version: 20170403201728) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "question_id"
