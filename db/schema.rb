95fabc62c2af66b0cb2e86cb2a1e550e0adb7ac2
danielmoralesp <daniel_moralesp@hotmail.com>
2017-04-05 23:08:40 -0500

    answers model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406034935) do
+ActiveRecord::Schema.define(version: 20170406040532) do
+
+  create_table "answers", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "question_id"
+    t.text     "body"
+    t.integer  "votes"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
+  add_index "answers", ["user_id"], name: "index_answers_on_user_id"
 
   create_table "questions", force: :cascade do |t|
     t.integer  "user_id"
