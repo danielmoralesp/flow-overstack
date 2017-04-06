6603dbf44272ccec72b12e1a5cf55e575af92da3
danielmoralesp <daniel_moralesp@hotmail.com>
2017-04-05 23:31:35 -0500

    custom, answer, users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406041051) do
+ActiveRecord::Schema.define(version: 20170406042740) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "user_id"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 20170406041051) do
     t.datetime "updated_at",  null: false
     t.text     "description"
     t.integer  "votes"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",            limit: 50
+    t.string   "email",           limit: 50
+    t.string   "password_digest"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,44 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-50.times do |n|
-		title= Faker::Lorem.sentence(1)
-		description= Faker::Lorem.sentence(3)
-		Question.create!(user_id: n,
-			             title: title,
-			             description: description,
-			             created_at:Time.zone.now)
+
+#Users
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password_digest:     "foobar",
+             created_at: Time.zone.now)
+
+
+60.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password_digest:   password,
+               created_at: (70-n).minutes.ago)
 
 end
+
+
+#Questions
+	50.times do |n|
+			title= Faker::Lorem.sentence(1)
+			description= Faker::Lorem.sentence(5)
+			Question.create!(user_id: n,
+				             title: title,
+				             votes: 0,
+				             description: description,
+				             created_at:(51-n).minutes.ago)
+	end
+
+# Answers
+	50.times do |n|
+	  body = Faker::Lorem.sentence(5)
+        Answer.create!(body: body,
+					   user_id: n,
+					   question_id: n,
+					   votes: 2,
+					   created_at: Time.zone.now)
+
+	end
