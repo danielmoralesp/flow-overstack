833275e0137ff8aa65c185d8f9637b644abb48f9
danielmoralesp <daniel_moralesp@hotmail.com>
2017-04-05 22:58:09 -0500

    crear nueva pregunta y correo seed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,30 @@ class QuestionsController < ApplicationController
   def index
 		@questions=Question.all
 	end
+
+	def show
+		@question= Question.find(params[:id])
+	end
+
+	def new
+		@question=Question.new
+	end
+
+	def create
+      @question= Question.new(question_params)
+        if @question.save
+           redirect_to questions_path, notice:'La pregunta ha sido creada con exitó'
+        else
+ 	       render :new
+        end
+	end
+
+
+private
+
+ def question_params
+
+ 	params.require(:question).permit(:title, :description)
+
+ end
 end
